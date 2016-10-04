cb5aa4ff5041895e27720d9b590c3e1999d496ac
Ricardo Perez <ricardoperez@ricardos-mac.local>
2016-10-04 10:33:46 -0500

    Initial Commit

---
+++ Codificacion Nombre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Codificacion Nombre
+//
+//  Created by Ricardo Perez on 10/4/16.
+//  Copyright © 2016 Asymc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
